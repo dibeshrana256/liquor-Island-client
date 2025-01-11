5e9f79eb0e27bbee2db579cca763af5434c6045a
DIBESHPC\broad <dibeshrana.dr@gmail.com>
2025-01-10 23:08:24 -0600

    added navbar and working on carasoul in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { NgFor } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { ProductsService } from '../services/products/products.service';
 import { Product } from '../models/product';
+import { HomeComponent } from "../pages/home/home.component";
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
