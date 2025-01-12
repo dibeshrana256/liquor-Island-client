c8b7679668493dfef0b35d997f32d3581791e23b
DIBESHPC\broad <dibeshrana.dr@gmail.com>
2025-01-12 15:23:05 -0600

    added hero section in the home page created an empty component for featured items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,28 +5,27 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from '../services/products/products.service';
 import { Product } from '../models/product';
-import { HomeComponent } from "../pages/home/home.component";
+import { HomeComponent } from '../pages/home/home.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'LiquorIslandClient';
 
-  constructor(private httpClient:HttpClient, 
-    private productsService:ProductsService){}
+  constructor(
+    private httpClient: HttpClient,
+    private productsService: ProductsService
+  ) {}
 
-  products:Product[]=[];
+  products: Product[] = [];
 
-  
-
-  ngOnInit(){
-
-    this.productsService.getProducts().subscribe(data => {
+  ngOnInit() {
+    this.productsService.getProducts().subscribe((data) => {
       this.products = data;
     });
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../pages/home/home.component';
+import { FeaturedComponent } from '../pages/featured/featured.component';
 
 export const routes: Routes = [
-    {
-        path: 'home', component: HomeComponent
-    },
-    {
-        path: '', redirectTo: "/home", pathMatch: "full"
-    }
-
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'featured',
+    component: FeaturedComponent,
+  },
 ];
